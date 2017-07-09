933e525edea8fbebc77fd5644d31d6aa18534f30
Allen Lantz <allen.m.lantz@gmail.com>
2017-07-08 20:25:10 -0500

    Organized all files

---
+++ assets/js/mathhulk.js
@@ -0,0 +1,44 @@
+function replaceAllTheThings() {
+    
+    document.title = "SpigotMC: Mathhulk Edition";
+    
+    var st = document.createElement("style");
+        if(st != null) {
+        st.type = "text/css";
+        st.rel = "stylesheet";
+        st.innerHTML = httpGet("chrome-extension://"+chrome.runtime.id+"/assets/css/mathhulk.css");
+        document.body.insertBefore(st, document.body.firstChild);
+    }
+    
+    var sp = document.getElementById("Sponsors");
+    if(sp != null) {
+        sp.innerHTML = httpGet("chrome-extension://"+chrome.runtime.id+"/assets/html/sponsors.html");
+    }
+    
+    var imgs = document.getElementsByTagName('img');
+    for (i = 0; i < imgs.length; i++) {
+        if (imgs[i].title == "Funny" || imgs[i].alt == "Funny") {
+            imgs[i].src = "chrome-extension://"+chrome.runtime.id+"/assets/img/mathhulk.png";
+            imgs[i].style = "background: url('') no-repeat 0px -128px; width: 16px; height: 16px;";
+        } else if (imgs[i].title == "Agree" || imgs[i].alt == "Agree") {
+            imgs[i].src = "chrome-extension://"+chrome.runtime.id+"/assets/img/methhulk.png";
+            imgs[i].style = "background: url('') no-repeat 0px -128px; width: 16px; height: 16px;";
+        } else if (imgs[i].title == "Informative" || imgs[i].alt == "Informative") {
+            imgs[i].src = "chrome-extension://"+chrome.runtime.id+"/assets/img/mathhulk_standing.png";
+            imgs[i].style = "background: url('') no-repeat 0px -128px; width: 16px; height: 16px;";
+        } else if (imgs[i].alt == "SpigotMC - High Performance Minecraft") {
+            imgs[i].src = "chrome-extension://"+chrome.runtime.id+"/assets/img/mathhulk.png";
+        }
+    }
+}
+
+function httpGet(theUrl) {
+  var xmlHttp = null;
+
+  xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", theUrl, false );
+  xmlHttp.send( null );
+  return xmlHttp.responseText;
+}
+
+replaceAllTheThings();
